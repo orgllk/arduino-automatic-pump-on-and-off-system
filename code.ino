0493fa517f5cac072ffc7b4fc7fc9ea9dcd94d00
orgllk <jagatjyothirmoyorgllk@gmail.com>
2023-04-03 21:21:37 +0530

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,16 @@
+int senser = 14 ;
+void setup() {
+  // put your setup code here, to run once:
+	pinMode(senser, INPUT);  
+  pinMode(4,OUTPUT);
+	Serial.begin(9600);  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int s = digitalRead(senser);
+if (s == HIGH){
+  digitalWrite(4,HIGH);
+}
+}
+
