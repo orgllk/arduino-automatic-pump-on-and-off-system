5c3e350f9bfd1701fa1e085bc97fb6f449e94a78
orgllk <101724163+orgllk@users.noreply.github.com>
2023-04-07 22:18:53 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,9 +1,9 @@
 int senser = 13 ;
 void setup() {
   // put your setup code here, to run once:
-	pinMode(senser, INPUT);  
+  pinMode(senser, INPUT);  
   pinMode(4,OUTPUT);
-	Serial.begin(9600);  
+  Serial.begin(9600);  
 }
 
 void loop() {
