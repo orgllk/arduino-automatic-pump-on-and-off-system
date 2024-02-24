2af5c5dedf26f1beef0c57f3fa8ccf669bd84ebe
ORGLLK <jagatjyothirmoyorgllk@gmail.com>
2024-02-24 22:52:21 +0530

    1.2

--- code.ino
+++ code.ino
@@ -1,33 +1,84 @@
-int trigPin = 4;
-int echoPin = 12;
-int led = 7;
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(1,OUTPUT);
-  pinMode(2,INPUT);
-  pinMode(3,OUTPUT);
-  Serial.begin(9600);
-  pinMode(led, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+// made by orgllk 
+// 1.5
+// auto pump 
+#define echopin 12
+#define trigpin 4
+#define LED 7
+char val;
+long duration; 
+int distance; 
+void setup() 
+{
+pinMode(trigpin, OUTPUT); 
+pinMode (echopin, INPUT); 
+Serial.begin(9600); 
+pinMode (LED,OUTPUT);
+
 }
 
-void loop() {
-long duration, distance;
-digitalWrite(trigPin,HIGH);
-delayMicroseconds(1000);
-digitalWrite(trigPin, LOW);
-duration=pulseIn(echoPin, HIGH);
-distance =(duration/2)/29.1;
-Serial.print(distance);
-Serial.println("CM");
-delay(10);
-if((distance<=10))
-{
-digitalWrite(led, HIGH);
+void loop()
+{ 
+
+
+
+
+
+if (val ==('F')){ // STOP
+  
+  digitalWrite(LED,0);
+
+  digitalWrite(trigpin,LOW); 
+delayMicroseconds(2); 
+digitalWrite(trigpin,LOW); 
+delayMicroseconds(10); 
+digitalWrite(trigpin,LOW); 
+duration=pulseIn (echopin,HIGH); distance=duration* 0.034/2; Serial.println (distance);
+  while (Serial.available() > 0)
+  {
+  val = Serial.read();
+  Serial.println(val);
+  }
+  
+}else   { // ON 
+a();
+digitalWrite(trigpin,LOW); 
+delayMicroseconds(2); 
+digitalWrite(trigpin,HIGH); 
+delayMicroseconds(10); 
+digitalWrite(trigpin,LOW); 
+duration=pulseIn (echopin,HIGH); distance=duration* 0.034/2; Serial.println (distance);
+  while (Serial.available() > 0)
+  {
+  val = Serial.read();
+  Serial.println(val);
+  }
 }
-else if(distance>10)
-{
-digitalWrite(led, LOW);
+return a();
 }
+
+
+  
+
+
+
+   
+
+void a(){ // BRAIN
+     if (distance <= 10){
+  digitalWrite (LED,1);
+  Serial.write("Of  ");
+   }
+else {
+   digitalWrite (LED,0);
+   Serial.write("ON  ");
+
 }
+
+ 
+}
+
+ 
+
+
+
+
